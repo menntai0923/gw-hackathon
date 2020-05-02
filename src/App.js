89384ce78b7c7118a883c449d05401aaddd4064a
asasigure <mkmokymh@gmail.com>
2020-05-02 17:16:08 +0900

    add: お試しFLOCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./css/App.scss";
-import Component from "./component/Component";
 
 function App() {
   return (
     <div className='App'>
-      <header className='l-header p-header'>
-        <h1>Ateam</h1>
+      <header className='l-header c-header'>
+        <div className='l-justify-spaceBetween c-header__content'>
+          <div className='c-header__logo'>
+            <span>時雨記</span>
+          </div>
+          <nav className='c-header__menu'>
+            <ul className='l-justify-left c-menu'>
+              <li className='c-menu__list'>トップ</li>
+              <li className='c-menu__list'>アバウト</li>
+              <li className='c-menu__list'>記事検索</li>
+            </ul>
+          </nav>
+        </div>
       </header>
-      <Component></Component>
+      <div
+        style={{ width: "100%", height: 400, backgroundColor: "black" }}
+      ></div>
     </div>
   );
 }
